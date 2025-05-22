d6afc21c0e18db68f639edc7fd5d4f15196d023d
joelefrain <joelefrain-debian>
2025-05-21 21:55:22 -0500

    Remove ignored files from version control

--- .py_debug/check_matplotlib.py
+++
@@ -1,4 +0,0 @@
-import matplotlib.font_manager as fm
-for font in fm.findSystemFonts(fontpaths=None, fontext='ttf'):
-    if 'arial' in font.lower():
-        print(fm.FontProperties(fname=font).get_name(), '->', font)
